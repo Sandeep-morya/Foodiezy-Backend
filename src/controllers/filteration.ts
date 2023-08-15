b0ebc18da0629533b49b6aff6cd61fa873cb058f
Sandeep-morya <saabmaurya@gmail.com>
2023-08-15 16:43:29 +0530

    segregated the  logic & cost for two is pending

---
+++ src/controllers/filteration.ts
@@ -0,0 +1,75 @@
+﻿const getDynamicOptions = (url: URLSearchParams, options: object) => {
+	const sortParams = url.get("sortby");
+	const foodTypeParams = url.getAll("foodType");
+	const cuisineParams = url.getAll("cuisine");
+	const deliveryParams = url.getAll("delivery");
+	const ratingParams = url.getAll("rating");
+	const expoloreParams = url.getAll("explore");
+	const costForTwoParams = url.getAll("costForTwo");
+
+	const veg = checkForVeg(foodTypeParams);
+	const rating = checkForRating(ratingParams);
+	console.log({
+		sortParams,
+		foodTypeParams,
+		cuisineParams,
+		deliveryParams,
+		ratingParams,
+		expoloreParams,
+		costForTwoParams,
+	});
+
+	if (veg === 0) {
+		options = { ...options, veg: true };
+	}
+
+	if (veg === 1) {
+		options = { ...options, veg: false };
+	}
+
+	if (cuisineParams.length > 0) {
+		options = { ...options, cuisines: { $all: cuisineParams } };
+	}
+
+	if (rating) {
+		options = { ...options, rating };
+	}
+
+	if (deliveryParams.length > 0) {
+		options = { ...options, "delivery.time": { $lte: 20 } };
+	}
+
+	// '₹300 - ₹500', 'Greater than ₹500', 'Less than ₹300'
+
+	return options;
+};
+
+function checkForVeg(foodTypeParams: string[]) {
+	const paramsLength = foodTypeParams.length;
+	if (paramsLength === 1) {
+		if (foodTypeParams[0] === "Pure Veg") {
+			return 0;
+		} else {
+			return 1;
+		}
+	}
+	return 2;
+}
+
+function checkForRating(ratingParams: string[]) {
+	const paramsLength = ratingParams.length;
+	if (paramsLength > 0) {
+		if (ratingParams.includes("Ratings 3.5+")) {
+			return { $gte: 3.5 };
+		}
+		if (ratingParams.includes("Ratings 4.0+")) {
+			return { $gte: 4 };
+		}
+		if (ratingParams.includes("Ratings 4.5+")) {
+			return { $gte: 4.5 };
+		}
+	}
+	return false;
+}
+
+export default getDynamicOptions;
--- src/controllers/restaurants.ts
+++ src/controllers/restaurants.ts
@@ -7,6 +7,7 @@ import type {
 	IRestaurant,
 	Parent,
 } from "../types";
+import getDynamicOptions from "./filteration";
 
 const addRestaurants = async (
 	_: Parent,
@@ -28,80 +29,9 @@ const getRestaurants = async (
 	serviceAreaId = parent?._id || serviceAreaId;
 	page = page || 0;
 	limit = limit || 20;
-
 	const url = new URL(`https://example.com/path?${queryParams || ""}`)
 		.searchParams;
-
-	const sortParams = url.get("sortby");
-	const foodTypeParams = url.getAll("foodType");
-	const cuisineParams = url.getAll("cuisine");
-	const deliveryParams = url.getAll("delivery");
-	const ratingParams = url.getAll("rating");
-	const expoloreParams = url.getAll("explore");
-	const costForTwoParams = url.getAll("costForTwo");
-
-	console.log({
-		sortParams,
-		foodTypeParams,
-		cuisineParams,
-		deliveryParams,
-		ratingParams,
-		expoloreParams,
-		costForTwoParams,
-	});
-
-	const veg = (() => {
-		const paramsLength = foodTypeParams.length;
-		if (paramsLength === 1) {
-			if (foodTypeParams[0] === "Pure Veg") {
-				return 0;
-			} else {
-				return 1;
-			}
-		}
-		return 2;
-	})();
-
-	let options: object = { serviceAreaId };
-
-	if (veg === 0) {
-		options = { ...options, veg: true };
-	}
-
-	if (veg === 1) {
-		options = { ...options, veg: false };
-	}
-
-	if (cuisineParams.length > 0) {
-		options = { ...options, cuisines: { $all: cuisineParams } };
-	}
-
-	const rating = (() => {
-		const paramsLength = ratingParams.length;
-		if (paramsLength > 0) {
-			if (ratingParams.includes("Ratings 3.5+")) {
-				return { $gte: 3.5 };
-			}
-			if (ratingParams.includes("Ratings 4.0+")) {
-				return { $gte: 4 };
-			}
-			if (ratingParams.includes("Ratings 4.5+")) {
-				return { $gte: 4.5 };
-			}
-		}
-
-		return false;
-	})();
-
-	if (rating) {
-		options = { ...options, rating };
-	}
-
-	if (deliveryParams.length > 0) {
-		options = { ...options, "delivery.time": { $lte: 20 } };
-	}
-	// '₹300 - ₹500', 'Greater than ₹500', 'Less than ₹300'
-
+	const options = getDynamicOptions(url, { serviceAreaId });
 	const totalCount = await Restaurant.countDocuments(options);
 	const documents = await Restaurant.find(options)
 		.skip(page * limit)
